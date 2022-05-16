68efbffb8232df3c19f660467cfeaa814c179a1a
konnan933 <konnan933@gmail.com>
2022-05-16 14:58:45 +0200

    Java projekt alap szerkezete

---
+++ TorpedoProjekt/src/main/Jatek.java
@@ -0,0 +1,26 @@
+
+package main;
+
+public class Jatek {
+   private int[] alapPalya;
+   private int palyaHossz;
+   private int[] holVan;
+   private int[] holTalalt;
+   
+   
+   private void setHajo(){
+       
+   }
+   public Boolean talalt_e(){
+       return null;
+       
+   }
+   
+   public Boolean elsuljedt_e(){
+       return null;
+       
+   }
+   
+    
+    
+}
