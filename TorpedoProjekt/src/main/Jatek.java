ce295106cbecda56a4c7b9cb01adfe863241f74d
konnan933 <konnan933@gmail.com>
2022-05-16 15:19:57 +0200

    setHajo() bemenet() és importok plusz meg egy adattaghajoHossz

--- TorpedoProjekt/src/main/Jatek.java
+++ TorpedoProjekt/src/main/Jatek.java
@@ -1,16 +1,36 @@
 
+
+
 package main;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class Jatek {
    private int[] alapPalya;
    private int palyaHossz;
    private int[] holVan;
    private int[] holTalalt;
+   private int hajoHossz;
+   
    
    
    private void setHajo(){
-       
+       Random rnd = new Random();
+       int hajoKezdet =  rnd.nextInt(palyaHossz-hajoHossz);
+       for (int i = hajoKezdet; i < hajoKezdet+3; i++) {
+           alapPalya[i] = 1;
+       }
    }
+   
+   public int bemenet(){
+       Scanner scr = new Scanner(System.in);
+       System.out.println("Hova lövi a torpedót?(1-7)");
+       int tipp = scr.nextInt()-1;
+       
+       return tipp;
+   }
+   
    public Boolean talalt_e(){
        return null;
        
